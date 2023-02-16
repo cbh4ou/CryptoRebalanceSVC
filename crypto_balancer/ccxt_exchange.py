8e2a34ac155c9c4c0e003aa836a72d502317eec2
Connor Hatcher <chatcher@loomissayles.com>
2023-02-15 22:22:33 -0600

    updated portfolio balance/routes, few bugs, but almost done

--- crypto_balancer/ccxt_exchange.py
+++ crypto_balancer/ccxt_exchange.py
@@ -1,6 +1,7 @@
+from typing import List
 import ccxt
 
-from functools import lru_cache
+from functools import lru_cache, reduce
 
 exchanges = ccxt.exchanges
 
@@ -15,7 +16,7 @@ class CCXTExchange():
         self.exch.requests_trust_env = True
         self.do_cancel_orders = do_cancel_orders
         self.exch.load_markets()
-        #self.cancel_orders
+        # self.cancel_orders
 
     @property
     @lru_cache(maxsize=None)
@@ -39,6 +40,33 @@ class CCXTExchange():
         total_balance = self.exch.fetch_balance()['total']
         return {total_balance[base] for base in total_balance.keys() if total_balance[base] != 0}
 
+    def get_portfolio_total(self, destination_code) -> float:
+        balance = self.exch.fetch_balance()
+        total_destination_value = 0
+        for code, quantity in balance['total'].items():
+            if code == destination_code:
+                total_destination_value += quantity
+            else:
+                routes = self.get_smart_route(code, destination_code)
+                print(routes)
+                if routes:
+                    total_destination_value += self.convert_routes_to_cost(
+                        quantity, routes)
+        return total_destination_value
+
+    def convert_routes_to_cost(self, init_quantity: float, routes: dict):
+        init_ticker = self.exch.fetch_ticker(routes[0]['symbol'])
+        init_cost = (init_quantity * init_ticker["last"])
+        if init_cost == 0:
+            return 0
+        if len(routes) > 1:
+            dest_ticker = self.exch.fetch_ticker(routes[0]['symbol'])
+            return init_cost / dest_ticker["last"]
+        else:
+            return init_cost
+
+        # ADA/USDT (100 * .4) = $40, ETH/USDT Cost
+
     @property
     @lru_cache(maxsize=None)
     def get_held_assets(self):
@@ -51,18 +79,69 @@ class CCXTExchange():
         return self.exch.calculate_fee(symbol=symbol, type=type,
                                        side=side, amount=amount, price=price)
 
-    @property
-    @lru_cache(maxsize=None)
-    def get_matched_pairs(self, pairs : list) -> list[str]:
+    def get_specific_pairs(self, pairs):
         active_pairs = []
         for i in pairs:
             for j in pairs:
-                pair = self.format_currency(i, j)
-                if pair in self.exch.markets and self.exch.markets[pair]['active']:
+                pair = self.format_symbol(i, j)
+                if pair in self.exch.markets.keys() and self.exch.markets[pair]['active']:
                     active_pairs.append(pair)
         return active_pairs
 
-    def format_currency(base : str, quote: str) -> str:
+    def get_all_held_symbols(self):
+        symbols = self.get_held_assets
+        active_symbols = []
+        for i in symbols:
+            for j in symbols:
+                symbol = self.format_symbol(i, j)
+                if symbol in self.exch.markets.keys() and self.exch.markets[symbol]['active']:
+                    active_symbols.append(symbol)
+        return active_symbols
+
+    def get_smart_route(self, starting_code, destination_code) -> list[dict]:
+        # will return ordered pairs that we will need to sell to get to our destination
+        sell_symbol = self.format_symbol(starting_code, destination_code)
+        buy_symbol = self.format_symbol(destination_code, starting_code)
+        symbols = self.exch.markets.values()
+        grouped_symbols = {}
+        symbols = [symbol for symbol in self.exch.markets.values()
+                   if symbol['active']]
+        {grouped_symbols.setdefault(item['symbol'], []).append(
+            item) for item in symbols}
+        sell_ticker = grouped_symbols.get(sell_symbol, None)
+        buy_ticker = grouped_symbols.get(buy_symbol, None)
+        if not [symbol for symbol in symbols
+                if symbol['base'] == starting_code]:
+            return None
+        elif not sell_ticker and not buy_ticker:
+            quote_routes = [symbol['quote']
+                            for symbol in symbols if symbol['base'] == starting_code]
+            trade_options = [symbol for symbol in symbols if (symbol["quote"] ==
+                                                              destination_code or symbol["base"] == destination_code) and (symbol["quote"] in
+                                                                                                                           quote_routes or symbol["base"] in quote_routes)]
+            is_buy = trade_options[0]['base'] == destination_code
+            return self.build_trade_routes(starting_code, trade_options[0], is_buy, quote_routes)
+        elif buy_ticker:
+            return [{"direction": "buy", "symbol": buy_symbol}]
+        else:
+            return [{"direction": "sell", "symbol": sell_symbol}]
+
+    def build_trade_routes(self, starting_code: str, route_to_destination: dict, is_buy: bool, quote_routes: list[str]) -> list[dict]:
+        # STMX -> USDT -> ADA
+        base, quote = (route_to_destination["base"], route_to_destination["quote"]) if route_to_destination[
+            "quote"] in quote_routes else (route_to_destination["quote"], route_to_destination["base"])
+        routes = [{'symbol': self.format_symbol(
+            starting_code, quote), 'direction': 'sell'}]
+        if is_buy:
+            routes.append({'symbol': self.format_symbol(
+                base, quote), 'direction': 'buy'})
+        else:
+            routes.append({'symbol': self.format_symbol(
+                quote, base), 'direction': 'sell'})
+        return routes
+
+    @staticmethod
+    def format_symbol(base: str, quote: str) -> str:
         return "{}/{}".format(base, quote)
 
     @property
@@ -88,6 +167,9 @@ class CCXTExchange():
                             'low': low, }
 
         return _rates
+
+    def get_lowest_fee_option(self, options: list[dict]):
+        pass
 
     @property
     @lru_cache(maxsize=None)
@@ -144,6 +226,6 @@ class CCXTExchange():
                     self.exch.cancel_order(order['id'], order['symbol'])
                     cancelled_orders.append(order)
             return cancelled_orders
-        
+
     def get_portfolio_balance() -> float:
         return 0.0
--- crypto_balancer/main.py
+++ crypto_balancer/main.py
@@ -47,10 +47,8 @@ def main(args=None):
         logger.error("Targets format invalid")
         sys.exit(1)
 
-    total_target = sum(targets.values())
-    if total_target != 100:
-        logger.error("Total target needs to equal 100, it is {}"
-                     .format(total_target))
+    if sum(targets.values()) != 100:
+        logger.error("Total target needs to equal 100")
         sys.exit(1)
 
     valuebase = config.get('valuebase') or args.valuebase
@@ -59,90 +57,11 @@ def main(args=None):
                             config['api_key'],
                             config['api_secret'])
 
-    print("Connected to exchange: {}".format(exchange.name))
-
-    if args.cancel:
-        print("Cancelling open orders...")
-        for order in exchange.cancel_orders():
-            print("Cancelled order:", order['symbol'], order['id'])
-
-    threshold = float(config['threshold'])
-    max_orders = int(args.max_orders)
-
-    portfolio = Portfolio.make_portfolio(
-        targets, exchange, threshold, valuebase)
-
-    print("Current Portfolio:")
-    for currency in portfolio.balances:
-        bal = portfolio.balances[currency]
-        pct = portfolio.balances_pct[currency]
-        tgt = targets[currency]
-        # print("  {:<6s} {:<8.2f} ({:>5.2f} / {:>5.2f}%)"
-        #     .format(currency, bal, pct, tgt))
-
-    print("  Total value: {:.2f} {}".format(portfolio.valuation_quote,
-                                            portfolio.quote_currency))
     balancer = SimpleBalancer()
-    executor = TradeExecutor(portfolio, exchange, balancer)
+    executor = TradeExecutor(exchange, balancer)
     executor_res = executor.run(force=args.force,
                                 trade=args.trade,
-                                max_orders=max_orders,
                                 mode=args.mode)
-
-    print("  Balance RMS error: {:.2g} / {:.2g}".format(
-        executor_res['initial_portfolio'].balance_rms_error,
-        threshold))
-
-    print("  Balance Max error: {:.2g} / {:.2g}".format(
-        executor_res['initial_portfolio'].balance_max_error,
-        threshold))
-
-    if not portfolio.needs_balancing and not args.force:
-        print("No balancing needed")
-        sys.exit(0)
-
-    print("Balancing needed{}:".format(" [FORCED]" if args.force else ""))
-
-    print("Proposed Portfolio:")
-    portfolio = executor_res['proposed_portfolio']
-
-    if not portfolio:
-        print("Could not calculate a better portfolio")
-        sys.exit(0)
-
-    for currency in portfolio.balances:
-        bal = portfolio.balances[currency]
-        pct = portfolio.balances_pct[currency]
-        tgt = targets[currency]
-        print("  {:<6s} {:<8.2f} ({:>5.2f} / {:>5.2f}%)"
-              .format(currency, bal, pct, tgt))
-
-    print("  Total value: {:.2f} {}".format(portfolio.valuation_quote,
-                                            portfolio.quote_currency))
-    print("  Balance RMS error: {:.2g} / {:.2g}".format(
-        executor_res['proposed_portfolio'].balance_rms_error,
-        threshold))
-
-    print("  Balance Max error: {:.2g} / {:.2g}".format(
-        executor_res['proposed_portfolio'].balance_max_error,
-        threshold))
-
-    total_fee = '%s' % float('%.4g' % executor_res['total_fee'])
-    print("  Total fees to re-balance: {} {}"
-          .format(total_fee,
-                  portfolio.quote_currency))
-
-    print("Orders:")
-    if args.trade:
-        for order in executor_res['success']:
-            print("  Submitted: {}".format(order))
-
-        for order in executor_res['errors']:
-            print("  Failed: {}".format(order))
-            print("  Error Response: {}".format(order))
-    else:
-        for order in executor_res['orders']:
-            print("  " + str(order))
 
 
 if __name__ == '__main__':
