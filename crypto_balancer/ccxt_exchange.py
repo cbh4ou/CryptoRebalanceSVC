380cb890ca77870efc8888641a1e97015c6ae227
Connor Hatcher <chatcher@loomissayles.com>
2023-07-09 16:44:53 -0500

    update reqs

--- crypto_balancer/ccxt_exchange.py
+++ crypto_balancer/ccxt_exchange.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import Dict, List, Optional, Union
 import ccxt
 
 from functools import lru_cache, reduce
+
+from crypto_balancer.order import Order
 
 exchanges = ccxt.exchanges
 
@@ -46,7 +48,7 @@ class CCXTExchange():
         total_balance = self.exch.fetch_balance()['total']
         return {total_balance[base] for base in total_balance.keys() if total_balance[base] != 0}
 
-    def get_portfolio_total(self, destination_code: str, mode: str) -> float:
+    def get_portfolio_total(self, destination_code: str, mode: str) -> Dict[str, Union[float, Dict[str, float]]]:
         codes = {}
         balance = self.exch.fetch_balance()
         total_destination_value = 0
@@ -57,25 +59,23 @@ class CCXTExchange():
             else:
                 routes = self.get_smart_route(code, destination_code)
                 if routes:
-                    value = self.convert_route_to_cost(
-                        quantity, routes, mode)
+                    value = self.convert_route_to_cost(quantity, routes, mode)
                     total_destination_value += value
                     codes[code] = value
-                    # print(code, routes, quantity, value,
-                    #    total_destination_value)
         return {"total": total_destination_value, "codes": codes}
 
-    def convert_route_to_cost(self, init_quantity: float, routes: dict, mode: str) -> float:
+    def convert_route_to_cost(self, init_quantity: float, routes: List[Dict[str, Union[str, bool]]], mode: str) -> float:
         init_cost = self.get_rate(routes[0]['symbol'])[mode]
-        init_value = (init_quantity * (1 / init_cost)) if routes[0]["direction"] == 'buy' else (
-            init_quantity * init_cost)
+        init_value = (init_quantity * (1 / init_cost)
+                      ) if routes[0]["direction"] == 'buy' else (init_quantity * init_cost)
         if init_value == 0:
             return 0
         if len(routes) > 1:
-            dest_ticker_price = self.get_rate(
-                routes[1]['symbol'])[mode]
-            cost = init_value / dest_ticker_price if routes[1]["direction"] == 'buy' else init_value / (
-                1 / dest_ticker_price)
+            dest_ticker_price = self.get_rate(routes[1]['symbol'])[mode]
+            if routes[1]["direction"] == 'buy':
+                cost = init_value / dest_ticker_price
+            else:
+                cost = init_value * dest_ticker_price
             return cost
         else:
             return init_value
@@ -111,20 +111,19 @@ class CCXTExchange():
                     active_symbols.append(symbol)
         return active_symbols
 
-    def get_smart_route(self, starting_code, destination_code) -> list[dict]:
+    def get_smart_route(self, starting_code: str, destination_code: str) -> Optional[List[Dict[str, Union[str, bool]]]]:
         # will return ordered pairs that we will need to sell to get to our destination
         sell_symbol = self.format_symbol(starting_code, destination_code)
         buy_symbol = self.format_symbol(destination_code, starting_code)
         grouped_symbols = {}
         symbols = [symbol for symbol in self.exch.markets.values()
                    if symbol['active']]
-        {grouped_symbols.setdefault(item['symbol'], []).append(
-            item) for item in symbols}
+        for item in symbols:
+            grouped_symbols.setdefault(item['symbol'], []).append(item)
 
-        sell_ticker = grouped_symbols.get(sell_symbol, None)
-        buy_ticker = grouped_symbols.get(buy_symbol, None)
-        if not [symbol for symbol in symbols
-                if symbol['base'] == starting_code] and not sell_ticker and not buy_ticker:
+        sell_ticker = grouped_symbols.get(sell_symbol)
+        buy_ticker = grouped_symbols.get(buy_symbol)
+        if not [symbol for symbol in symbols if symbol['base'] == starting_code] and not sell_ticker and not buy_ticker:
             return None
         elif buy_ticker:
             return [{"direction": "buy", "symbol": buy_symbol}]
@@ -133,16 +132,14 @@ class CCXTExchange():
         else:
             quote_routes = [symbol['quote']
                             for symbol in symbols if symbol['base'] == starting_code]
-            trade_options = [symbol for symbol in symbols if (symbol["quote"] ==
-                                                              destination_code or symbol["base"] == destination_code) and (symbol["quote"] in
-                                                                                                                           quote_routes or symbol["base"] in quote_routes)]
+            trade_options = [symbol for symbol in symbols if (symbol["quote"] == destination_code or symbol["base"] == destination_code) and (
+                symbol["quote"] in quote_routes or symbol["base"] in quote_routes)]
             is_buy = trade_options[0]['base'] == destination_code
             return self.build_trade_routes(starting_code, trade_options[0], is_buy, quote_routes)
 
-    def build_trade_routes(self, starting_code: str, route_to_destination: dict, is_buy: bool, quote_routes: list[str]) -> list[dict]:
-        # STMX -> USDT -> ADA
-        base, quote = (route_to_destination["base"], route_to_destination["quote"]) if route_to_destination[
-            "quote"] in quote_routes else (route_to_destination["quote"], route_to_destination["base"])
+    def build_trade_routes(self, starting_code: str, route_to_destination: dict[str, str], is_buy: bool, quote_routes: List[str]) -> List[Dict[str, Union[str, bool]]]:
+        base, quote = (route_to_destination["base"], route_to_destination["quote"]) if route_to_destination["quote"] in quote_routes else (
+            route_to_destination["quote"], route_to_destination["base"])
         routes = [{'symbol': self.format_symbol(
             starting_code, quote), 'direction': 'sell'}]
         if is_buy:
@@ -158,8 +155,8 @@ class CCXTExchange():
         return "{}/{}".format(base, quote)
 
     @lru_cache(maxsize=None)
-    def get_rates(self) -> dict[dict]:
-        _rates = {}
+    def get_rates(self) -> Dict[str, Dict[str, float]]:
+        rates = {}
         tickers = self.tickers
         for pair in tickers:
             ticker = tickers[pair]
@@ -171,13 +168,11 @@ class CCXTExchange():
                 high = orderbook['asks'][0][0]
                 low = orderbook['bids'][0][0]
             mid = (high + low) / 2.0
-            _rates[pair] = {'mid': mid,
-                            'high': high,
-                            'low': low, }
+            rates[pair] = {'mid': mid, 'high': high, 'low': low}
 
-        return _rates
+        return rates
 
-    def get_rate(self, symbol: str) -> dict[dict]:
+    def get_rate(self, symbol: str) -> dict[str, float]:
         ticker = self.tickers[symbol]
         if 'ask' in ticker and 'bid' in ticker:
             high = ticker['ask']
@@ -187,7 +182,7 @@ class CCXTExchange():
             high = orderbook['asks'][0][0]
             low = orderbook['bids'][0][0]
         mid = (high + low) / 2.0
-        return {'mid': mid, 'high': high, 'low': low, }
+        return {'mid': mid, 'high': high, 'low': low}
 
     def get_lowest_fee_option(self, options: list[dict]):
         pass
@@ -201,42 +196,32 @@ class CCXTExchange():
     def fee(self):
         return self.exch.fees['trading']['maker']
 
-    def preprocess_order(self, order):
+    def preprocess_order(self, order: Order) -> Optional[Order]:
         try:
             limits = self.limits[order.pair]
         except KeyError:
             return None
 
         order.amount = float(
-            self.exch.amount_to_precision(
-                order.pair, order.amount))
+            self.exch.amount_to_precision(order.pair, order.amount))
         order.price = float(
-            self.exch.price_to_precision(
-                order.pair, order.price))
+            self.exch.price_to_precision(order.pair, order.price))
 
         if order.price == 0 or order.amount == 0:
             return None
 
-        if order.amount < limits['amount']['min'] \
-           or order.amount * order.price < limits['cost']['min']:
+        if order.amount < limits['amount']['min'] or order.amount * order.price < limits['cost']['min']:
             return None
-        order.type_ = 'LIMIT'
-        return order
 
-    def check_limits(self, starting_code: str, destination_code: str, weight: float, mode: str, total: float):
-        trade_routes = self.get_smart_route(
-            starting_code, destination_code)
-        symbol = ""
-        if trade_routes:
-            symbol = trade_routes[-1]["symbol"]
-        else:
-            return False
+        order.type_ = 'LIMIT'
+        return order if order.amount >= limits['amount']['min'] else None
+
+    def check_limits(self, starting_code: str, destination_code: str, weight: float, mode: str, total: float) -> bool:
+        trade_routes = self.get_smart_route(starting_code, destination_code)
+        symbol = trade_routes[-1]["symbol"] if trade_routes else ""
         limit = self.get_limit(symbol)
         price = self.get_rate(symbol)[mode]
-        if (total * weight) / price < limit['amount']['min'] or abs(weight * total) < limit['cost']['min']:
-            return False
-        else:
-            return True
+        return not ((total * weight) / price < limit['amount']['min'] or abs(weight * total) < limit['cost']['min'])
 
     def execute_order(self, order):
         if not order.type_:
