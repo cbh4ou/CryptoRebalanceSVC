e76461cd1ea76399b6e0a944fee7c85a529e8b87
Connor Hatcher <chatcher@loomissayles.com>
2023-07-09 19:29:00 -0500

    clean up

--- crypto_balancer/rebalancer.py
+++ crypto_balancer/rebalancer.py
@@ -2,11 +2,13 @@ from itertools import product
 
 from crypto_balancer.order import Order
 from crypto_balancer.ccxt_exchange import CCXTExchange
+
 targets = {
-    'BTC' : 50,
-    'USDT' : 40,
-    'ETH' : 10
+    'BTC': 50,
+    'USDT': 40,
+    'ETH': 10
 }
+
 
 class Rebalancer():
 
@@ -17,8 +19,8 @@ class Rebalancer():
         weights = {key: value / total_balance for key, value in codes.items()}
         amounts_to_sell = {}
         print(weights)
-        print({"new_target" : weights[key] - value for key, value in targets.keys() if key in weights})
-            
+        print({"new_target": weights[key] - value for key,
+              value in targets.keys() if key in weights})
+
     def build_orders():
         pass
-            
